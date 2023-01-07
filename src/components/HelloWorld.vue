5e387661fd8384a9ff1fb6765ac91b587e5f6b00
Igor Ponso <igorponso07@gmail.com>
2023-01-07 14:20:35 -0300

    feat: add .styl files and use i18n translation file

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,13 +1,13 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+  import { ref } from 'vue';
 
-defineProps<{ msg: string }>()
+  defineProps<{ msg: string }>();
 
-const count = ref(0)
+  const count = ref(0);
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
+  <h1>{{ $t('greetings') }}</h1>
 
   <div class="card">
     <button type="button" @click="count++">count is {{ count }}</button>
@@ -31,8 +31,8 @@ const count = ref(0)
   <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
 
-<style scoped>
-.read-the-docs {
-  color: #888;
-}
+<style scoped lang="stylus">
+  .read-the-docs {
+    color: #888;
+  }
 </style>
